fe53032ee6cb34f56ecb86e29174f49d1ea8fec3
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-03-05 12:02:37 +0300

    task06

--- js/home.js
+++ js/home.js
@@ -1,16 +1,42 @@
+"use strict";
 
-let Name = prompt("Please enter your name:");
-let gender = prompt("Please enter your gender(male/female):");
-let age = prompt("Please enter your Age:");
-let welcomingMessage ="Weolcome to Todoist, ";
-if(age <= 0)
-alert("Age cant be Zero or less!");
+let questions = {};
+questions.name = "Please enter your name:";
+questions.gender = "Please enter your gender(male/female):";
+questions.age = "Please enter your Age:";
+questions.yesNO = [];
+questions.yesNO.push("Are you employed?");
+questions.yesNO.push("Do you use our app?");
+questions.yesNO.push("are you currently married?");
 
-if(gender=="male")
-  welcomingMessage+="Mr.";
- else if(gender=="female")
-welcomingMessage+="Ms.";
-welcomingMessage=welcomingMessage.concat(Name);
+let answers = {};
+answers.name = prompt(questions.name);
+answers.gender = prompt(questions.gender);
+answers.age = prompt(questions.age);
+answers.yesNO = [];
+
+let welcomingMessage = "Weolcome to Todoist, ";
+
+if (answers.age <= 0)
+  alert("Age cant be Zero or less!");
+
+if (answers.gender == "male")
+  welcomingMessage.concat("Mr.");
+else if (answers.gender == "female")
+  welcomingMessage.concat("Ms.");
+
+welcomingMessage = welcomingMessage.concat(answers.name);
 if (confirm("press OK to recive welcoming message")) {
   alert(welcomingMessage);
+}
+
+alert("Now we would like to ask you a couple of (yes/no) questions.");
+
+for (let index = 0; index < questions.yesNO.length; index++) {
+  answers.yesNO.push(prompt(questions.yesNO[index]));
+
+  if (answers.yesNO[index] != "yes" && answers.yesNO[index] != "no")
+    answers.yesNO[index] = "invalid";
+
+  console.log(questions.yesNO[index] + "\nAnswer:" + answers.yesNO[index]);
 }
